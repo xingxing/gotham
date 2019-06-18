eaccb94b7972dac460a23905f627426c28e87273
Xing Xing <xing@peatio.com>
2019-06-18 16:01:18 +0800

    chore: init

---
+++ lib/gotham.ex
@@ -0,0 +1,18 @@
+defmodule Gotham do
+  @moduledoc """
+  Documentation for Gotham.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Gotham.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/gotham_test.exs
@@ -0,0 +1,8 @@
+defmodule GothamTest do
+  use ExUnit.Case
+  doctest Gotham
+
+  test "greets the world" do
+    assert Gotham.hello() == :world
+  end
+end
